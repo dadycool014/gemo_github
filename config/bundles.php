b7e9d3abbefa0cc529c45415d8ddef5108cef8c4
dadycool014 <el_mehdi.ammar@yahoo.com>
2021-10-13 13:03:51 +0100

    Add bundle files

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
